539ae6e42e524107f1e876ac47566daa8181257b
Jérémy Morin <jer.morin@free.fr>
2015-01-24 16:08:22 +0100

    Article final commit

--- src/main/webapp/scripts/app/entities/people/people-detail.controller.js
+++ src/main/webapp/scripts/app/entities/people/people-detail.controller.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('miagebdxApp')
-    .controller('PeopleDetailController', function ($scope, $stateParams, People, Event, Article) {
+    .controller('PeopleDetailController', function ($scope, $stateParams, People, Event, ArticlePeople) {
         $scope.people = {};
         $scope.load = function (id) {
             People.get({id: id}, function(result) {
               $scope.people = result;
             });
+            $scope.articles = ArticlePeople.get({id: id});
         };
         $scope.load($stateParams.id);
     });
