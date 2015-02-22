66ec17dbd8c7496aef3c901ebb689bab188de80c
Jérémy Morin <jer.morin@free.fr>
2015-02-22 20:07:25 +0100

    website(fix) dateTime format

--- src/main/webapp/scripts/components/entities/article/article.service.js
+++ src/main/webapp/scripts/components/entities/article/article.service.js
@@ -5,7 +5,13 @@ angular.module('miagebdxApp')
         return $resource('api/articles/:id', {}, {
             'query': { method: 'GET', isArray: true},
             'get': {
-                method: 'GET'
+                method: 'GET',
+                transformResponse: function (data) {
+                    data = angular.fromJson(data);
+                    var data2form = data.date.split("-");
+                    data.date = new Date(new Date(data2form[0], data2form[1] - 1, data2form[2]));
+                    return data;
+                }
             }
         });
     });
